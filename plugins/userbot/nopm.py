7766b7e82c24f334307c62c3a8cce208a55830e9
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-10-22 21:19:48 +0530

    Add files via upload

---
+++ plugins/userbot/nopm.py
@@ -0,0 +1,30 @@
+
+import asyncio
+from pyrogram import Client, filters
+from utils import USERNAME
+from config import Config
+from pyrogram.errors import BotInlineDisabled
+
+msg=Config.msg
+REPLY_MESSAGE=Config.REPLY_MESSAGE
+
+@Client.on_message(filters.private & filters.incoming & ~filters.bot & ~filters.service & ~filters.me & ~filters.edited & ~filters.chat([777000, 454000]))
+async def nopm(client, message): 
+    try:
+        inline = await client.get_inline_bot_results(USERNAME, "SAF_ONE")
+        m=await client.send_inline_bot_result(
+            message.chat.id,
+            query_id=inline.query_id,
+            result_id=inline.results[0].id,
+            hide_via=True
+            )
+        old=msg.get(message.chat.id)
+        if old:
+            await client.delete_messages(message.chat.id, [old["msg"], old["s"]])
+        msg[message.chat.id]={"msg":m.updates[1].message.id, "s":message.message_id}
+    except BotInlineDisabled:
+            print(f"Inline Mode for @{USERNAME} is not enabled. Enable it from @Botfather to turn on PM Guard !")
+            await message.reply_text(f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@DC_LOGS | @KRISHNA_THULSI 👑</b>")
+    except Exception as e:
+        print(e)
+        pass
