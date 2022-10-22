94912899baec21184e5fe674003d22f4037a34ee
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-10-22 22:05:53 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-echo "Cloning Repo, Please Wait..."
-git clone https://github.com/sakhaavvaavaj93/krishnaprabha
-echo "Installing Requirements..."
-cd /krishnaprabha
-pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
-python3 main.py
